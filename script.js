bbc3feca8c2af6a413b555f9625e74c0c8d456ed
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-09-01 14:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// Function to download and display images
+async function downloadAndDisplayImages() {
+	for(const image of images) {
+	try	{
+			const response = await fetch(image.url);
+		if(response.ok) {
+			const imageUrl = URL.createObjectURL(await response.blob());
+			const imgElement = document.createElement("img");
+			imgElement.src = imageUrl;;
+			
+		}else {
+			throw new Error(`Failed to load image's URL: ${image.url}`);
+		}
+		} catch (error){
+		console.log(error);
+		}
+	}
+}
+
+btn.addEventListener("click",downloadAndDisplayImages );
